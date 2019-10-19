116c5e4f7022256620f7975e73d7cb8b8892ea3b
João Filipe Matos Silva <joao@kspace.pt>
2019-10-19 15:41:33 +0100

    onupdate

--- WebApplication3/Startup.cs
+++ WebApplication3/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -9,6 +10,9 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
 
 namespace WebApplication3
 {
@@ -17,9 +21,21 @@ namespace WebApplication3
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            BotClient = new TelegramBotClient(Configuration.GetValue<string>("TelegramToken"), new HttpClient());
+            BotClient.OnUpdate += BotClientOnOnUpdate;
+            BotClient.StartReceiving(allowedUpdates: new []{UpdateType.Message, UpdateType.ChannelPost});
+        }
+
+        private async void BotClientOnOnUpdate(object sender, UpdateEventArgs e)
+        {
+            await BotClient.SendTextMessageAsync(
+                chatId: e.Update.Message.Chat,
+                text: "You said:\n" + e.Update.Message.Text
+            );
         }
 
         public IConfiguration Configuration { get; }
+        public TelegramBotClient BotClient { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
